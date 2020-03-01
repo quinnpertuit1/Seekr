71f2023bd2f7e6d8307008d5f063fd8bb54550e8
russellJacob <23370169+russellJacob@users.noreply.github.com>
2020-02-29 20:16:43 -0500

    Initialize boto3 client

--- global_variables.py
+++ global_variables.py
@@ -1,8 +1,13 @@
 from profiles.scraper import LinkedInScraper
 import sys
+import boto3
+from constants import *
 
 # Need selenium installation
 linkedin_scraper = None
+
+#Initialize boto3 client
+s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 
 # if any(command in sys.argv for command in ['runserver']):
 #     linkedin_scraper = LinkedInScraper()
